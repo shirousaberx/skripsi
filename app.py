0a18168e466637d92e63d9aa68474f45779b0bd4
My name <me@example.com>
2024-04-02 14:19:18 +0700

    Finished all functionalities
    Note: the tensorflow model and colab artifacts are still tentative

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ import os
 import time
 import json
 import pandas as pd
+import tensorflow as tf
+import numpy as np
+
+# for plotting
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, accuracy_score
 import nltk
 import seaborn as sns
 
+# for text preprocessing
 import re
 from indoNLP.preprocessing import remove_html, remove_url, replace_slang, replace_word_elongation, remove_stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
@@ -21,6 +26,13 @@ app.secret_key = '@#$123456&*()'
 ALLOWED_EXTENSIONS = {'csv'}
 app.config['UPLOAD_FOLDER_CSV'] = os.path.join(os.getcwd(), 'static/csv')
 app.config['UPLOAD_FOLDER_IMAGES'] = os.path.join(os.getcwd(), 'static/images')
+
+# load model tensorflow
+model = tf.keras.models.load_model('model')
+
+model.compile(optimizer=tf.keras.optimizers.Adam(0.0005),
+                  loss=tf.keras.losses.BinaryCrossentropy(),
+                  metrics=['accuracy', tf.keras.metrics.F1Score(average='micro', threshold=0.5)])
 
 # buat direktori untuk menyimpan csv yang diupload
 if not os.path.isdir(app.config['UPLOAD_FOLDER_CSV']): 
@@ -77,6 +89,8 @@ def preprocess_text(text):
     return text
 
 # Function untuk menghandle upload file csv pada form evaluasi dan upload csv
+# output: filename csv (concat(session id + filename)) yang disimpan
+# Note: harus dalam konteks request
 def handle_upload():
     # check if the post request has the file part
     if 'file' not in request.files:
@@ -99,6 +113,33 @@ def handle_upload():
 
     return filename
 
+# Membuat plot word_frequency dan menyimpan gambarnya di disk
+# Input: dataframe dengan kolom 'preprocessed_content' dan filename dengan ekstensi
+# output: filename gambar (concat(session id + filename)) yang disimpan
+def create_word_frequency(df, filename):
+    # For every token that is in stopwords, remove it
+    # df.map(str)     # Make sure text is string utf-8 encoding, or remove_stopwords might err
+
+    text = ' '.join(remove_stopwords(str(text)) for text in df).split()
+
+    # # Using nltk find all the frequencies
+    text_freq = nltk.FreqDist(text).most_common(10)
+    text_freq = pd.Series(dict(text_freq))
+    fig, ax = plt.subplots(figsize=(10,10))
+
+    ## Seaborn plotting using Pandas attributes + xtick rotation for ease of viewing
+    posiif_plot = sns.barplot(x=text_freq.index, y=text_freq.values, ax=ax)
+    plt.xticks(rotation=30)
+    plt.xlabel('Kata')
+    plt.xlabel('Frekuensi')
+
+    filename = session['uuid'] + '-' + filename
+    frekuensi_filepath = os.path.join(app.config['UPLOAD_FOLDER_IMAGES'], filename)
+    plt.savefig(frekuensi_filepath)
+
+    return filename
+
+
 # =================================================================
 
 # ========================== Middleware ===========================
@@ -110,58 +151,28 @@ def hook():
         session['uuid'] = str(uuid.uuid4())
 
 # =================================================================
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    
+    if request.method == 'POST':
+        hasil_preprocessing = preprocess_text(request.form['ulasan'])
+        output_model = model.predict([hasil_preprocessing])[0][0]
+        hasil_klasifikasi = 'POSITIF' if output_model >= 0.5 else 'NEGATIF'
+
+        return render_template('index.jinja2',
+                ulasan=request.form['ulasan'],
+                hasil_preprocessing=hasil_preprocessing,
+                output_model=output_model if hasil_preprocessing != '' else 'INVALID',
+                hasil_klasifikasi=hasil_klasifikasi if hasil_preprocessing != '' else 'INVALID',
+                klasifikasi=True)
+
     return render_template('index.jinja2', klasifikasi=True)
 
 @app.route('/upload_csv', methods=['GET', 'POST'])
 def upload_csv():
     if request.method == 'POST':
-        filename = handle_upload()
-        df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER_CSV'], filename))
+        handle_upload()
 
-        ## Text positif
-        # For every token that is in stopwords, remove it
-        text_positif = ' '.join(remove_stopwords(text) for text in df[df['predicted_label'] == 1]['preprocessed_content']).split()
-
-        # # Using nltk find all the frequencies
-        text_positif_freq = nltk.FreqDist(text_positif).most_common(10)
-
-        text_positif_freq = pd.Series(dict(text_positif_freq))
-
-        fig, ax = plt.subplots(figsize=(10,10))
-
-        ## Seaborn plotting using Pandas attributes + xtick rotation for ease of viewing
-        posiif_plot = sns.barplot(x=text_positif_freq.index, y=text_positif_freq.values, ax=ax)
-        plt.xticks(rotation=30)
-        plt.xlabel('Kata')
-        plt.xlabel('Frekuensi')
-        frekuensi_positif_filepath = os.path.join(app.config['UPLOAD_FOLDER_IMAGES'], session['uuid'] + '-' + 'frekuensi_positif.png')
-        plt.savefig(frekuensi_positif_filepath)
-
-        ## text negatif
-        # For every token that is in stopwords, remove it
-        text_negatif = ' '.join(remove_stopwords(text) for text in df[df['predicted_label'] == 0]['preprocessed_content']).split()
-
-        # # Using nltk find all the frequencies
-        text_negatif_freq = nltk.FreqDist(text_negatif).most_common(10)
-
-        text_negatif_freq = pd.Series(dict(text_negatif_freq))
-
-        fig, ax = plt.subplots(figsize=(10,10))
-
-        ## Seaborn plotting using Pandas attributes + xtick rotation for ease of viewing
-        posiif_plot = sns.barplot(x=text_negatif_freq.index, y=text_negatif_freq.values, ax=ax)
-        plt.xticks(rotation=30)
-        plt.xlabel('Kata')
-        plt.xlabel('Frekuensi')
-        frekuensi_negatif_filepath = os.path.join(app.config['UPLOAD_FOLDER_IMAGES'], session['uuid'] + '-' + 'frekuensi_negatif.png')
-        plt.savefig(frekuensi_negatif_filepath)
-
-        return render_template('hasil_upload_csv.jinja2', upload_csv=True, 
-                frekuensi_positif=session['uuid'] + '-' + 'frekuensi_positif.png',
-                frekuensi_negatif=session['uuid'] + '-' + 'frekuensi_negatif.png')
+        return render_template('hasil_upload_csv.jinja2', upload_csv=True)
 
     return render_template('upload_csv.jinja2', upload_csv=True)
 
@@ -171,28 +182,31 @@ def hasil_upload_csv():
     if request.method == 'POST':
         filename = session['uuid'] + '-' + 'upload_csv.csv'
         df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER_CSV'], filename))
+        df = df[['content']]
+
+        df['preprocessed_content'] = df['content'].map(preprocess_text)
+        df = df[df['preprocessed_content'] != '']  # Jika content kosong setelah preprocessing, buang row
+
+        df['prediction'] = model.predict(np.array(df['preprocessed_content'])) # predict class
+        df['predicted_label'] = df.apply(lambda row: (1 if row['prediction'] >= 0.5 else 0), axis=1) # bulatkan prediksi
+
+        frekuensi_positif_filename = create_word_frequency(df[df['predicted_label'] == 1]['preprocessed_content'], 'frekuensi_positif.png')
+        frekuensi_negatif_filename = create_word_frequency(df[df['predicted_label'] == 0]['preprocessed_content'], 'frekuensi_negatif.png')
 
         result = df.to_json()
         parsed = json.loads(result)
+
+        parsed['frekuensi_positif'] = url_for('static', filename='images/' + frekuensi_positif_filename)
+        parsed['frekuensi_negatif'] = url_for('static', filename='images/' + frekuensi_negatif_filename)
 
         return json.dumps(parsed, indent=4)
 
 @app.route('/evaluasi', methods=['GET', 'POST'])
 def evaluasi():
     if request.method == "POST":
-        filename = handle_upload()
-        df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER_CSV'], filename))
+        handle_upload()
         
-        # confusion matrix
-        cm = confusion_matrix(df['label'], df['predicted_label'])
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-        disp.plot()
-
-        plt.title('confusion matrix')
-        confusion_matrix_filepath = os.path.join(app.config['UPLOAD_FOLDER_IMAGES'], session['uuid'] + '-' + 'confusion_matrix.png')
-        plt.savefig(confusion_matrix_filepath)
-
-        return render_template('hasil_evaluasi.jinja2', confusion_matrix=session['uuid'] + '-' + 'confusion_matrix.png')
+        return render_template('hasil_evaluasi.jinja2', evaluasi=True)
 
     return render_template('evaluasi.jinja2', evaluasi=True)
 
@@ -201,9 +215,30 @@ def hasil_evaluasi():
     if request.method == 'POST':
         filename = session['uuid'] + '-' + 'upload_csv.csv'
         df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER_CSV'], filename))
+        df = df[['content', 'score', 'label']]
+
+        df['preprocessed_content'] = df['content'].map(preprocess_text)
+        df = df[df['preprocessed_content'] != '']  # Jika content kosong setelah preprocessing, buang row
+
+        df['prediction'] = model.predict(np.array(df['preprocessed_content']), batch_size=1024) # predict class
+        df['predicted_label'] = df.apply(lambda row: (1 if row['prediction'] >= 0.5 else 0), axis=1) # bulatkan prediksi
+
+        # confusion matrix
+        cm = confusion_matrix(df['label'], df['predicted_label'])
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+        disp.plot()
+
+        plt.title('confusion matrix')
+        confusion_matrix_filename = session['uuid'] + '-' + 'confusion_matrix.png'
+        confusion_matrix_filepath = os.path.join(app.config['UPLOAD_FOLDER_IMAGES'], confusion_matrix_filename)
+        plt.savefig(confusion_matrix_filepath)
 
         result = df.to_json()
         parsed = json.loads(result)
+
+        parsed['binary_accuracy'] = f"{accuracy_score(df['label'], df['predicted_label']):.2%}"
+        parsed['f1_score'] = f"{f1_score(df['label'], df['predicted_label']):.2%}"
+        parsed['confusion_matrix'] = url_for('static', filename='images/' + confusion_matrix_filename);
 
         return json.dumps(parsed, indent=4)
 
